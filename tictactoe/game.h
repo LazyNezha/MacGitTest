25b770339fe342d84dd5584881eec51e827a35b7
lazynezha <noahxfyi@icloud.com>
2021-12-07 11:09:59 +0800

    三子棋练习

--- tictactoe/game.h
+++ tictactoe/game.h
@@ -2,6 +2,8 @@
 #define TICTACTOE_GAME_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define ROW 3
 #define COL 3
@@ -10,5 +12,11 @@
 void InitBoard (char board[ROW][COL], int row, int col);
 //打印棋盘
 void DisplayBoard (char board[ROW][COL], int row, int col);
+//玩家走
+void PlayerMove (char board[ROW][COL], int row, int col);
+//电脑走
+void ComputerMove (char board[ROW][COL], int row, int col);
+//判断输赢
+char IsWin (char board[ROW][COL], int row, int col);
 
 #endif //TICTACTOE_GAME_H
--- tictactoe/main.c
+++ tictactoe/main.c
@@ -14,10 +14,42 @@ void game() {
     InitBoard(board, ROW, COL);
     //打印棋盘
     DisplayBoard(board, ROW, COL);
+    char ret = 0;
+    while (1) {
+        //玩家走
+        PlayerMove(board, ROW, COL);
+        DisplayBoard(board, ROW, COL);
+        //玩家赢返回 - *
+        //电脑赢返回 - #
+        //平局返回 - Q
+        //继续游戏返回 - C
+        ret = IsWin(board, ROW, COL);
+        if (ret != 'C') {
+            break;
+        }
+        //电脑走
+        ComputerMove(board, ROW, COL);
+        DisplayBoard(board, ROW, COL);
+        ret = IsWin(board, ROW, COL);
+        if (ret != 'C') {
+            break;
+        }
+    }
+    if (ret == '*') {
+        printf("玩家赢了\n");
+    }
+    else if (ret == '#') {
+        printf("电脑赢了\n");
+    }
+    else {
+        printf("平局\n");
+    }
+    DisplayBoard(board, ROW, COL);
 }
 
 int main() {
     int input = 0;
+    srand((unsigned int)time(NULL));
     do {
         menu();
         printf("请输入：>");
